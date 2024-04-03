1f948eaeffeca44d838f440515e8fd6f532cd0bd
Preethi <preethi5014@gmail.com>
2024-04-03 08:02:41 +0800

    graphana

--- variables.tf
+++ variables.tf
@@ -6,13 +6,13 @@ variable "tools" {
 
       grafana = {
         instance_type        = "t3.small"
-        policy_resource_list = ["ec2:DescribeInstances"]
-      }
+        policy_resource_list = []
 
+      }
+    }
   }
 }
-
-variable "zone_id" {
+        variable "zone_id" {
   default = "Z04475551DM169036MV75"
 }
 
